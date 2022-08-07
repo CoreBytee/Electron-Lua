6e8877e02b0f38efd42759412c7f14114153f223
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-07 19:37:14 +0200

    Create package.info.lua

---
+++ Package/src/Main/resources/package.info.lua
@@ -0,0 +1,35 @@
+-- See https://github.com/Dot-lua/TypeWriter/wiki/package.info.lua-format for more info
+
+return { InfoVersion = 1, -- Dont touch this
+
+    ID = "Electron-Lua", -- A unique id 
+    Name = "Electron-Lua",
+    Description = "A Electron-Lua",
+    Version = "1.0.0",
+
+    Author = {
+        Developers = {
+            "CoreByte"
+        },
+        Contributors = {}
+    },
+
+    Dependencies = {
+        Luvit = {
+            "creationix/coro-spawn"
+        },
+        Git = {},
+        Dua = {}
+    },
+
+    Contact = {
+        Website = "https://cubic-inc.ga",
+        Source = "https://github.com/Dot-lua/TypeWriter/",
+        Socials = {}
+    },
+
+    Entrypoints = {
+        Main = "Electron.Test"
+    }
+
+}
