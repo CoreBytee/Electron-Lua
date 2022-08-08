6a6b2167710cafdeb020e1d77b0d7e316e28d545
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-08-08 10:10:56 +0200

    main lua file

--- Package/src/Main/lua/Electron/Main.lua
+++ Package/src/Main/lua/Electron/Main.lua
@@ -1,8 +1,93 @@
-print("Hello World!")
-p(TypeWriter.ApplicationData)
 TypeWriter.Runtime.LoadFile(TypeWriter.ApplicationData .. "/Electron-Lua/IPC-Bootstrap.twr")
-local Import("openipc.bootstrap").LoadAll()
+TypeWriter.Runtime.LoadFile(TypeWriter.ApplicationData .. "/Electron-Lua/Get-Node.twr")
+TypeWriter.Runtime.LoadInternal("BetterEmitter")
+local NodePath, NodeVersion = Import("ga.corebyte.get-node").Download()
+local IPClient = Import("openipc.bootstrap").LoadAll()
+local Emitter = Import("ga.corebyte.BetterEmitter")
+local FS = require("FS")
 
 local Data = {
     SessionId = string.random(32),
+    IsDev = TypeWriter.ArgumentParser:GetArgument("is-dev", "is-dev", "false") == "true",
+    ElectronLocation = TypeWriter.ApplicationData .. "/Electron-Lua/Electron/",
 }
+if Data.IsDev == true then
+    Data.ElectronLocation = "./Electron/"
+end
+Data.IPC = IPClient:new("Electron-" .. Data.SessionId, "Main")
+local IPC = Data.IPC
+
+local Spawn = require("coro-spawn")
+if FS.existsSync(Data.ElectronLocation .. "/node_modules/") == false then
+    TypeWriter.Logger.Info("Installing node dependencies...")
+    local Result = Spawn(
+        NodePath .. "/npm.cmd",
+        {
+            args = {
+                "install",
+                "--production"
+            },
+            cwd = Data.ElectronLocation,
+            stdio = {
+                process.stdin.handle,
+                process.stdout.handle,
+                process.stderr.handle,
+            }
+        }
+    )
+end
+
+local MadeObjects = {}
+IPC:RegisterMessage(
+    "ObjectEvent",
+    function(Message)
+        local Object = MadeObjects[Message.ObjectId]
+        if Object ~= nil then
+            Object:Emit(Message.Name, table.unpack(Message.Data))
+        end
+    end
+)
+
+local ReadyEmitter = Emitter:new()
+IPC:RegisterMessage(
+    "AppReady",
+    function ()
+        ReadyEmitter:Emit("ready")
+    end
+)
+
+local Result, Error = Spawn(
+    NodePath .. "/npm.cmd",
+    {
+        args = {
+            "run", "start", require("base64").encode(require("json").encode(
+                {
+                    SessionId = Data.SessionId,
+                    IsDev = Data.IsDev,
+                    ElectronLocation = Data.ElectronLocation,
+                }
+            ))
+        },
+        cwd = Data.ElectronLocation,
+        stdio = {
+            process.stdin.handle,
+            process.stdout.handle,
+            process.stderr.handle,
+        }
+    }
+)
+ReadyEmitter:WaitFor("ready")
+
+local Objects = {
+    BrowserWindow = Import("Electron.Objects.BrowserWindow")
+}
+local ObjectWrappers = {}
+for Index, Object in pairs(Objects) do
+    ObjectWrappers[Index] = function (...)
+        local NewId = string.random(64)
+        local NewObject = Object:new(Data, NewId, ...)
+        MadeObjects[NewId] = NewObject
+        return NewObject
+    end
+end
+return ObjectWrappers
