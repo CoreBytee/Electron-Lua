b4ccb87b89f353eaecacf3aed61da8e79dee50df
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-15 16:51:17 +0200

    2.0.0

--- src/Main/resources/package.info.lua
+++ src/Main/resources/package.info.lua
@@ -5,7 +5,7 @@ return { InfoVersion = 1, -- Dont touch this
     ID = "Electron-Lua", -- A unique id 
     Name = "Electron-Lua",
     Description = "A Electron-Lua",
-    Version = "1.0.0",
+    Version = "2.0.0",
 
     Author = {
         Developers = {
@@ -17,6 +17,7 @@ return { InfoVersion = 1, -- Dont touch this
     Dependencies = {
         Luvit = {
             "creationix/coro-spawn",
+            "creationix/coro-fs",
             "creationix/base64"
         },
         Git = {},
