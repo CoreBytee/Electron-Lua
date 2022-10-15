ce5c61368ce347fe6c49dba87e6c49b827182727
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-10-15 16:50:54 +0200

    remove bootstrap

--- src/Bootstrap/lua/Electron/bootstrap/Main.lua
+++
@@ -1,4 +0,0 @@
-return {
-    Download = Import("Electron.bootstrap.Stages.Download"),
-    LoadAll = Import("Electron.bootstrap.Stages.LoadAll"),
-}
--- src/Bootstrap/lua/Electron/bootstrap/Test.lua
+++
@@ -1,1 +0,0 @@
-Import("Electron.bootstrap").LoadAll()
