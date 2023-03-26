6e602c10c24bb4a498b7dab026f99a80681f1bfd
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-03-26 22:42:52 +0200

    Update Test.js

--- src/Main/js/electronhelper/Test.js
+++ src/Main/js/electronhelper/Test.js
@@ -1,3 +1,18 @@
 const Electron = Import("electronhelper")
+const App = Electron.app
+const BrowserWindow = Electron.BrowserWindow
 
-console.log(Electron)
+App.on("ready", function() {
+    console.log("Ready")
+    const Window = new BrowserWindow(
+        {
+            width: 800,
+            height: 600,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        }
+    )
+    Window.loadURL("https://www.google.com")
+})
+//console.log(Electron)
