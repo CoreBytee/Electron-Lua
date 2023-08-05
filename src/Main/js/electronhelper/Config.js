1df017545f8e28f2d6899043dda6fc4b8b5e1755
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-08-06 00:29:28 +0200

    new ver

--- src/Main/js/electronhelper/Config.js
+++ src/Main/js/electronhelper/Config.js
@@ -1,5 +1,5 @@
 const Config = {}
-Config.ElectronVersion = "23.1.4"
+Config.ElectronVersion = "25.2.0"
 Config.ElectronPath = require("path").resolve(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Modules/electron/Versions/${Config.ElectronVersion}/`)
 
 module.exports = Config
