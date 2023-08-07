a1455b9c8a98ca077b2b03b72c24ad980f94df91
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2023-08-07 12:06:59 +0200

    updoot electron

--- src/Main/js/electronhelper/Config.js
+++ src/Main/js/electronhelper/Config.js
@@ -1,5 +1,5 @@
 const Config = {}
-Config.ElectronVersion = "25.2.0"
+Config.ElectronVersion = "25.4.0"
 Config.ElectronPath = require("path").resolve(`${TypeWriter.Folder}/Cache/ModuleCache/NPM/Modules/electron/Versions/${Config.ElectronVersion}/`)
 
 module.exports = Config
